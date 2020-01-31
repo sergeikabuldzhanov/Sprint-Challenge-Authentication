53b0723b92149b6cbcd97e998b56042ca248f560
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-31 14:23:33 +0500

    Enpoints

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,38 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const userDb = require("./users-model");
+const bcryptjs = require("bcryptjs");
+const makeToken = require("./make-token");
 
-router.post('/register', (req, res) => {
+router.post("/register", async (req, res, next) => {
   // implement registration
+  const { username, password } = req.body;
+  try {
+    const hashedPassword = await bcryptjs.hash(password, 12);
+    const newUser = await userDb.insert({ username, password: hashedPassword });
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", async (req, res, next) => {
   // implement login
+  const credentials = req.body;
+  try {
+    const user = await userDb.getByUsername(credentials.username);
+    const passwordIsCorrect = await bcryptjs.compare(
+      credentials.password,
+      user.password
+    );
+    if (passwordIsCorrect) {
+      const token = makeToken(user);
+      res.status(200).json({ message: `Access granted!`, token });
+    } else {
+      res.status(401).json({ message: `Unauthorized` });
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
